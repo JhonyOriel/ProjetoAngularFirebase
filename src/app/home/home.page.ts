142f77beb111cb1ac293ac5ff80838dc639815a4
Jhony Oriel de Barros Timoteo <162513709+JhonyOriel@users.noreply.github.com>
2024-09-03 23:51:35 +0000

    03.09.2024

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
+import { Router } from '@angular/router';
+import { NOMEM } from 'dns';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +13,36 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  paises: any;
+  isLoading: boolean = true;
+
+  constructor(
+    private router: Router
+  ){
+    this.getPaisCode();
+  }
+
+  getPaisCode(){
+    this.isLoading = true;
+    fetch('https://restcountries.com/v3.1/all?fields=name,ccn3,flags')
+    .then( dados => dados.json() )
+    .then(dados => {
+      console.log(dados);
+      this.paises = dados;
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally( () => {
+      this.isLoading = false;
+    } )
+  }
+
+  verDetalhes(ccn3: string){
+    this.router.navigate(['/detalhe-pais'], {
+      state: { codigo: ccn3 }
+    });
+  }
+
+
 }
